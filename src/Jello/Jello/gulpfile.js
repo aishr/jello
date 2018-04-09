38e3b2eae5a1e9a971619e31fc6679381eb0cfa9
Josh Wolfe <jwolfe@indigo.ca>
2018-04-09 16:01:21 -0400

    Added gulp for SASS compilation

---
+++ src/Jello/Jello/gulpfile.js
@@ -0,0 +1,22 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+"use strict";
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var webroot = "./ClientApp/";
+var paths = {
+    scss: webroot + "scss/**/site.scss",
+    scssDest: webroot + "css/"
+};
+
+gulp.task('default', function () {
+    // place code for your default task here
+    gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.scssDest));
+});
